31959933a8b7a831fe46a1d6eddae23a98ed6801
Дмитрий <dapavlov16@yandex.ru>
2020-07-08 10:19:15 +0300

    organize folders

--- MovieProject/ViewController.swift
+++ MovieProject/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        
+        let service = NetworkService()
+        service.searchMovie("Snatch") { [weak self] (response) in
+            guard let results = response.results else {
+                return
+            }
+            DispatchQueue.main.async {
+                self?.label.text = results[0].overview ?? "error"
+            }
+        }
     }
-
+    
+    private func setupView() {
+        label = UILabel()
+        view.addSubview(label)
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20, weight: .thin)
+        label.numberOfLines = 0
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
 
 }
 
