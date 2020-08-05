2f1bdf2c4c3cab1058d25030add82014fe026c31
Дмитрий <dapavlov16@yandex.ru>
2020-08-05 11:02:03 +0300

    Update TMDBResponse.swift

--- MovieProject/Service/Network/TMDBResponse.swift
+++ MovieProject/Service/Network/TMDBResponse.swift
@@ -8,7 +8,7 @@
 
 struct TMDBResponse: Decodable {
     var page: Int?
-    var results: [Results]?
+    var results: [Result]?
     var totalResults: Int?
     var totalPages: Int?
     
@@ -20,7 +20,7 @@ struct TMDBResponse: Decodable {
     }
 }
 
-struct Results: Decodable {
+struct Result: Decodable {
     var posterPath: String?
     var adult: Bool?
     var overview: String?
